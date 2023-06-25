6f0d454e90bb644b5110cda3c79b2b998842caa3
marawanthedev <61912691+marawanthedev@users.noreply.github.com>
2023-06-25 20:18:40 +0800

    Adding jest ts setup

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.ts?$": "ts-jest",
+  },
+  transformIgnorePatterns: ["<rootDir>/node_modules/"],
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.jest.json",
+    },
+  },
+  setupFilesAfterEnv: ["./src/jest.setup.ts"],
+};
