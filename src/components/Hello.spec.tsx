7c4efbe8a2cb12a3f9cd245f54bf723fab97eb73
marawanthedev <61912691+marawanthedev@users.noreply.github.com>
2023-06-25 22:18:07 +0800

    updating test cases for hello

--- src/components/Hello.spec.tsx
+++ src/components/Hello.spec.tsx
@@ -1,8 +1,8 @@
-import { render,screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Hello from "./Hello";
 
 it("renders Hello", () => {
   render(<Hello />);
-  const myElement=screen.getByText('Hello')
-  expect(myElement).toBeInTheDocument()
+  const myElement = screen.getByText("Hellos");
+  expect(myElement).toBeInTheDocument();
 });
